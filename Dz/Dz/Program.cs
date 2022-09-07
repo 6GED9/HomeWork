1b2f6752eed8666d9e948d9b45aaef23b6a57f24
wudisluv <74738263+6GED9@users.noreply.github.com>
2022-09-08 01:04:05 +0300

    Dz

---
+++ Dz/Dz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dz
+{
+    class Shop
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Aspirant("fas","fasf","fsaf",3);
+            Student[] students = new[] {new Student("ФИО 1", "Фамилия1", "Группа 1", 5),
+                                        new Aspirant("ФИО 2", "Фамилия2", "Группа 2", 3)};
+            foreach(var student in students)
+            {
+                Console.WriteLine(student.firstName + " " + student.lastName + " " + student.Group + " " + student.averageMask + " " + student.getScholarship());
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
